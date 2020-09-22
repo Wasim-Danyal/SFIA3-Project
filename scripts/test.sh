3543cb487ad41abfcb06027bf83f601b64ef0155
Steven Bore <smtbore@gmail.com>
2020-09-22 11:42:38 +0100

    more scripts, ammended docker script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
+
+# install npm
+sudo apt install npm -y
+sudo npm uninstall -g angular-cli @angular/cli
+sudo npm cache clean --force
+sudo npm install -g @angular/cli@8.0.3
+sudo npm install --save-dev @angular/cli@8.0.3
+sudo rm ./front-end/package-lock.json
+sudo npm install
+
+# test the front-end using angular unit testing
 cd front-end
 ng test --watch=false --browsers=ChromeHeadless
