0cb29270bc47ff9a15bab366955c5e7ac537ac2d
Steven Bore <smtbore@gmail.com>
2020-09-23 15:58:15 +0100

    removed sudo from docker command, fixed on the vm

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,6 +2,6 @@
 export SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URI}
 export SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 export SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
-sudo docker-compose build
-sudo docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-sudo docker-compose push
+docker-compose build
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker-compose push
