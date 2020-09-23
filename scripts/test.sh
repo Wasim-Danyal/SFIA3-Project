936b404d4fbf4b9e218f459424bbe0001231bf4a
Steven Bore <smtbore@gmail.com>
2020-09-23 12:11:10 +0100

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
-
-npm install -g @angular/cli@8.0.3
-npm install --save-dev @angular/cli@8.0.3
 rm ./front-end/package-lock.json
 npm install
 
 # test the front-end using angular unit testing
 cd front-end
 ng test --watch=false --browsers=ChromeHeadless
+cd ..
