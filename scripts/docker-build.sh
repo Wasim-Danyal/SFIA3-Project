db561558026adae51d950ca0a343cb04b663e068
Steven Bore <smtbore@gmail.com>
2020-09-23 16:13:44 +0100

    put sudo for all docker commands

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,6 +2,6 @@
 export SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URI}
 export SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 export SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
-docker-compose build
+sudo docker-compose build
 sudo docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-docker-compose push
+sudo docker-compose push
