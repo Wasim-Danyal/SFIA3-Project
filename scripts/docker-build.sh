65dbdd363703988f7a768d97fd9537f8a2e8963a
Steven Bore <smtbore@gmail.com>
2020-09-24 20:01:37 +0100

    updated docker script

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+docker system prune
+docker rmi $(docker images -a -q)
 export SPRING_DATASOURCE_URI="${SPRING_DATASOURCE_URI}"
 export SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}"
 export SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}"
