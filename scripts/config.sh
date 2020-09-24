32f47434baf72afc473dfb0e9ff3b706e6c8e3a3
Steven Bore <smtbore@gmail.com>
2020-09-24 14:07:20 +0100

    changed kubectl commands

--- scripts/config.sh
+++ scripts/config.sh
@@ -19,7 +19,7 @@ sudo mv terraform ./bin
 # install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl 
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 
 # install docker and docker-compose
--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-sudo kubectl apply -f kubernetes/backend.yaml
-sudo kubectl apply -f kubernetes/frontend.yaml
-sudo kubectl apply -f kubernetes/mysql.yaml
-sudo kubectl apply -f kubernetes/nginx.yaml
-sudo kubectl apply -f kubernetes/nginx-config-map.yaml
+kubectl apply -f kubernetes/backend.yaml
+kubectl apply -f kubernetes/frontend.yaml
+kubectl apply -f kubernetes/mysql.yaml
+kubectl apply -f kubernetes/nginx.yaml
+kubectl apply -f kubernetes/nginx-config-map.yaml
 sleep 20
-sudo kubectl get services
+kubectl get services
